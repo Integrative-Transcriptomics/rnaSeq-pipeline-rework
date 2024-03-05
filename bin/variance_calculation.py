ef4df1fa702f0887450c1ad30bb2aaf6e3559494
Sarina Stadler <sarina@sarinas-macbook.speedport.ip>
2024-03-05 11:32:53 +0100

    Added TPM calculation to tabel

--- bin/variance_calculation.py
+++ bin/variance_calculation.py
@@ -10,46 +10,48 @@ import math
 import sys, getopt
 from glob import glob
 import gff_parser
-
-""" 
-command for execution:
-python3 variance_calculation.py -genesf /Users/sarina/Documents/Bachelorarbeit/rnaSeq-pipeline-rework/rRNA_genes_type.txt 
--fcf /Users/sarina/Documents/Bachelorarbeit/rnaSeq-pipeline-rework/rRNADepletion/counts.txt
-"""
+import bin.tpm_calculation as tpm
 
 # calculates the empirical variance
 def calculate_empirical_variance(rRNA_genes_type, counts_txt):
     
+    # Datasets 
     genes_data = [] #[[gene_id, type], [gene_id, type], ...]
     counts_data = [] # [[Geneid, Chr, Start, End, Strand, Length, gene_name, sample1, sample2, ...], [..., ..., ..., ...]];field 7 -> end 
     not_rRNA_genes = []
     
+    # Values in TPM format
+    tpm_values = tpm.tpm_calculation(counts_txt)
+
     with open(rRNA_genes_type) as genes_file:
         for line in genes_file:
             gene, type = line.strip().split(",")
             genes_data.append(gene)
-    
-    # Add all non rRNA genes to counts_data     
+            
+    counter = -2
+            
     with open(counts_txt) as counts_file:
         for line in counts_file:
             current_line = line.strip().split('\t')
             if current_line[0] not in genes_data[1:]:
-                if not current_line[0].startswith("#"):
-                    not_rRNA_genes.append(current_line[0])
-                    counts_data.append(current_line)
-
-    not_rRNA_genes = not_rRNA_genes[1:]
-    mean_values = []
-
+                if not current_line[0].startswith('#'):
+                    if counter < 0 :
+                        counts_data.append(current_line)
+                    else:
+                        counts_data.append([current_line[0]] + tpm_values[counter])
+            counter += 1
+            
     number_of_genes = len(counts_data)
-    number_of_samples = len(counts_data[0][7:])#
+    number_of_samples = len(counts_data[0][1:])
     
+    mean_values = []    
     variance_calculated_genes = []
     
-    # 1) Caclulate mean value
+    
+    # 1) Calculate mean value
     for i in range(1, number_of_genes):
         sum = 0
-        for value in counts_data[i][7:]:
+        for value in counts_data[i][1:]:
             sum += float(value)
             
         mean = round(sum/number_of_samples, 2)
@@ -57,8 +59,8 @@ def calculate_empirical_variance(rRNA_genes_type, counts_txt):
         # Limit for minimum expression rate: 10
         if mean > 10:
             mean_values.append(mean)
-            variance_calculated_genes.append(counts_data[i][0])     # When the mean expression rate is above 10 save it to the mean values
-                
+            variance_calculated_genes.append(counts_data[i][0])
+            
     empirical_variance = []
     standard_deviation = []
     count = 0
@@ -67,43 +69,35 @@ def calculate_empirical_variance(rRNA_genes_type, counts_txt):
     for i in range(1, number_of_genes):
         sum = 0
         variance = 0
+        
         if counts_data[i][0] in variance_calculated_genes:
-            for value in counts_data[i][7:]:
+            for value in counts_data[i][1:]:
                 sum += (float(value) - mean_values[count]) ** 2
-            
-            variance = round(sum/(number_of_samples-1), 2)
+                
+            variance = round(sum/(number_of_samples - 1), 2)
             empirical_variance.append(variance)
             
-            standard_deviation.append(round(math.sqrt(variance),2))
-            count += 1 
-    
-    coefficient_of_variation = []        
+            standard_deviation.append(round(math.sqrt(variance), 2))
+            count += 1
+            
+    coefficient_of_variation = []
     # 3) Calculate coefficient of variation
     for std, mean in zip(standard_deviation, mean_values):
         coefficient_of_variation.append(round(std/mean, 2))
-    
-    # print(len(variance_calculated_genes))
-   
+        
     results = [[a,b,c,d,e] for a,b,c,d,e in zip(mean_values, empirical_variance, standard_deviation, coefficient_of_variation, variance_calculated_genes)]
+        
+    # Sort results in two different ways to find subset
+    # 1) Expression rate from highest do lowest (mean value, with TPM calculation)
+    sorted_mean = sorted(results, key=lambda x: (x[0]), reverse=True)
     
-    # Sort resulst in two different ways and find subset
-    # 1) Expression rate from highest to lowest (mean value)
-    sorted_results_mean_expression = sorted(results, key=lambda x: (x[0]), reverse=True)
-    first_100_genes = sorted_results_mean_expression[:100]
+    first_100_genes = sorted_mean[:100]
     
-    # 2) Variance from lowest to highest (empirical variance)
-    # sorted_results_variance = sorted(results, key=lambda x: (x[1]))
-    sorted_first_100_genes_after_variance = sorted(first_100_genes, key=lambda x: (x[1]))
+    #2) Variance from lowest to highest (empirical variance)
+    sorted_variance_first_100 = sorted(first_100_genes, key=lambda x: (x[1]))
     
-    # Limit to find intersection: 2200 (in this case only two elements are the same) 
-    #intersection = [element for element in sorted_results_mean_expression[:2200] if element in sorted_results_variance[:2200]]
-    
-    # Ouput of the genes with the smallest variance but high expression
-    #print(sorted_first_100_genes_after_variance[:10])
-    return(sorted_first_100_genes_after_variance[:10])
-    
-    # print(intersection)
-    # Create csv Datei with all values calculated in calculate_empirical_variance
+    return(sorted_variance_first_100[:10])
+            
 def create_table(rRNA_genes_type, counts_txt, gff_file):
     #["mean_values" , "empirical_variance", "standard_deviation", "coefficient_of_variation", "gene"]
     header_values = ['Mean', 'Standard deviation', 'Coefficient of variation', 'Gene ID', 'Product']
